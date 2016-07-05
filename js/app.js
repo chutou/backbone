d438d6e6f2dbbab5c67f69fb1ce221eb64b45175
zhouwy <zhouwy@landicorp.com>
2016-07-05 15:54:01 +0800

    todo views

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+var ENTER_KEY = 13;
+var ESC_KEY = 27;
 
+$(function () {
+	'use strict';
+
+	// 实例化app,就会调用构造函数，app.AppView 的initialize；
+	new app.AppView();
+});
